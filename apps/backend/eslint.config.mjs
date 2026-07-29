a897425b876b4208ba262ff2ebcbd2be2e249b3a
Karuzoo Alexander <kalex.codr@gmail.com>
2026-07-29 19:29:39 -0300

    [0.1.31] fix linter with new config

---
+++ apps/backend/eslint.config.mjs
@@ -0,0 +1,21 @@
+import config from '@rocketseat/eslint-config/node.mjs'
+import neostandard, { resolveIgnoresFromGitignore } from 'neostandard'
+
+export default [
+  ...config,
+  ...neostandard({
+    ignores: resolveIgnoresFromGitignore(),
+    ts: true,
+  }),
+  {
+    rules: {
+      camelcase: 'off',
+      '@stylistic/max-len': ['off', {
+        code: 80,
+        tabWidth: 2,
+        ignoreUrls: true,
+        ignoreComments: false,
+      }],
+    },
+  },
+]
